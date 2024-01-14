06c5458287650ef49eed77eb0b45534c96bacc79
k-narusawa <swkei320@gmail.com>
2024-01-14 15:03:22 +0900

    init

---
+++ src/main/kotlin/com/knarusawa/secure_stream/SecureStreamApplication.kt
@@ -0,0 +1,11 @@
+package com.knarusawa.secure_stream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecureStreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecureStreamApplication>(*args)
+}
---
+++ src/test/kotlin/com/knarusawa/secure_stream/SecureStreamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knarusawa.secure_stream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecureStreamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
