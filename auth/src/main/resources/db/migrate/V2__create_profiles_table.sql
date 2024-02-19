ddae6b87a96fdf0c5d0e300086308968da9f11c3
narusawakohei <swkei320@gmail.com>
2024-02-19 21:48:42 +0900

    refactor: 💡 クエリ修正

--- auth/src/main/resources/db/migrate/V2__create_profiles_table.sql
+++ auth/src/main/resources/db/migrate/V2__create_profiles_table.sql
@@ -1,10 +1,12 @@
 CREATE TABLE profiles
 (
-    user_id        VARCHAR(255) PRIMARY KEY,
+    user_id        VARCHAR(255),
     family_name    VARCHAR(255) NOT NULL,
     given_name     VARCHAR(255) NOT NULL,
     nickname       VARCHAR(255),
     picture        VARCHAR(255),
     created_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
