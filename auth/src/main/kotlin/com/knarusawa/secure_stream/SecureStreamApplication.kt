ba074da761b3d9143ab15da0bce532100c187631
narusawakohei <swkei320@gmail.com>
2024-02-21 08:10:16 +0900

    wrench: 🔧 共通モジュール呼び出しまで

--- auth/src/main/kotlin/com/knarusawa/secure_stream/SecureStreamApplication.kt
+++ auth/src/main/kotlin/com/knarusawa/secure_stream/SecureStreamApplication.kt
@@ -2,8 +2,10 @@ package com.knarusawa.secure_stream
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.knarusawa"])
 class SecureStreamApplication
 
 fun main(args: Array<String>) {
