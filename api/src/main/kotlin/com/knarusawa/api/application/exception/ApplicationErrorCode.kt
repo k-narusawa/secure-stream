94aad76133eeb288164e9afa98cee377c012b6cb
k-narusawa <narusawakohei@team-lab.com>
2024-03-06 11:02:36 +0900

    wrench: 🔧 error修正

--- api/src/main/kotlin/com/knarusawa/api/application/exception/ApplicationErrorCode.kt
+++ api/src/main/kotlin/com/knarusawa/api/application/exception/ApplicationErrorCode.kt
@@ -1,13 +1,13 @@
 package com.knarusawa.api.application.exception
 
-import org.openapitools.model.Model400ErrorError
+import org.openapitools.model.ErrorError
 
 enum class ApplicationErrorCode(
-    val code: Model400ErrorError.Code,
+    val code: ErrorError.Code,
     val message: String
 ) {
     USER_NOT_FOUND(
-        code = Model400ErrorError.Code.ID400_E001,
+        code = ErrorError.Code.ID400_E001,
         message = "対象のユーザーが見つかりませんでした"
     ),
 }
